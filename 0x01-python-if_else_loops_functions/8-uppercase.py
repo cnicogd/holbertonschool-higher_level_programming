a0be69d8a2a1ac30b74b1bb76704c01353383fbd
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-04-28 00:50:22 -0500

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         upperword = str[i]
-        if ord(str[i]) > 96 and ord(str[i] < 123):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
             upperword = chr(ord(str[i]) - 32)
-            print("{}".format(upperword), end='')
+        print("{}".format(upperword), end='')
     print()
