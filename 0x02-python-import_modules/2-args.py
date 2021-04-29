57cf324ed5e8ee248848e0b25925e6ad56f64a15
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-04-28 22:38:52 -0500

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     import sys
-    if len(sys.arg) == 1:
+    if len(sys.argv) == 1:
         print("0 arguments.")
 
     elif len(sys.argv) == 2:
