d0198a847b9c06f1b4b832ae2bcf37f1210e24d6
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-05-18 23:03:01 -0500

    squares

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,13 @@
 """square class"""
 
 class Square:
-""" square class"""
+"""Square class init, assign a value to
+        Args:
+            size (int): The private arg size of the square.
+        Raise:
+            ValueError: If size is less than Zero
+            TypeError: If size is not an integer
+        """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
