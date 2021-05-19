d9090e320ff85435663b4de80026a47649a34e37
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-05-18 22:52:14 -0500

    squares-git

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""0-square empty class Square that defines a square"""
+
+
 class Square:
+    """Square class assigns"""
     def __init__(self, size):
         self.__size = size
