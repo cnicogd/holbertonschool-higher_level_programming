524fe403a4f44d7ec0daa4f31893fe5ca0e4cd47
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-05-19 19:14:36 -0500

    print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
     try:
-        num = 0
-        for i in my_list:
-            if num < x:
-                print("{}".format(i), end='')
-                num += 1
-        print('')
-        return num
-    except SyntaxError:
-        print("Error Syntax")
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
