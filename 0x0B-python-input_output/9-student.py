45255e08536e544aacf79c963e3ec9ae3ec3ae43
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-06-01 00:33:59 -0500

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module thatconstruct a student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ Constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Class Dictionary """
+        return self.__dict__.copy()
