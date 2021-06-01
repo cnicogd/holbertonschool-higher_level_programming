cf996d591280c1487616e2d5a7a65ccfd1e9933c
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-05-31 23:19:15 -0500

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that append a file"""
+
+
+def append_write(filename="", text=""):
+    """ function that appends a string returns the number of characters """
+
+    with open(filename, "a", encoding='utf-8') as f:
+        return f.write(text)
