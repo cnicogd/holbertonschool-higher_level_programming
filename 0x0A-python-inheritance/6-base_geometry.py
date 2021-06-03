29f897029e47d4abf1610764891ddfd4dbf8ad3c
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-06-02 22:48:27 -0500

    geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Geometry class"""
+
+
+class BaseGeometry:
+    """ Geometry Class """
+    def area(self):
+        """ Geometry """
+        raise Exception("area() is not implemented")
