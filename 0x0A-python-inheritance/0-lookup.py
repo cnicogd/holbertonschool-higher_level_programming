3871ad7e08b32ed0fa539d9058c7f9bd2065b04d
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-06-02 22:09:07 -0500

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module taht checks the atributes"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+    """
+    return dir(obj)
