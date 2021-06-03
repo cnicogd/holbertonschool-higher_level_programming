be6d2822d01b943ef18e01fae35649135016573e
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-06-02 22:26:59 -0500

    instances

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""returns an object"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function that returns True if the object is an instance
+    """
+    return isinstance(obj, a_class)
