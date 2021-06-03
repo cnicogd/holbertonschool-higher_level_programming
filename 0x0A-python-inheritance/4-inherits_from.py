09e56688d9998de612223ac42771eb3eccf326ce
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-06-02 22:36:22 -0500

    inherit-from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""an instance of a class that inherited"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance
+    of a class that inherited (directly or indirectly)
+    from the specified class.
+    """
+    if type(obj) == a_class:
+        return False
+    else:
+        return isinstance(obj, a_class)
